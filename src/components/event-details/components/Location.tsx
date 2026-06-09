892e88b7ef1e89e583e2bae570377498b9db1d41
Isaacjosh23 <isaacjoshua969@gmail.com>
2026-06-10 00:16:25 +0100

    little corrections

--- src/components/event-details/components/Location.tsx
+++ src/components/event-details/components/Location.tsx
@@ -1,3 +1,4 @@
+import RightDiagonalArrowIcon from "@/ui/icons/right-diagonal-arrow";
 import { DetailCard } from "./types";
 
 interface LocationProps {
@@ -14,11 +15,11 @@ function Location({ detail }: LocationProps) {
         href={detail.mapUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="font-serif text-[1.6rem] text-(--color-text-inverse) underline decoration-(--color-gold) decoration-[0.5px] underline-offset-4 hover:text-(--color-gold) transition-colors duration-300 leading-[1.4]"
+        className="font-serif text-[1.6rem] text-(--color-text-inverse) underline decoration-(--color-gold) decoration-[0.5px] underline-offset-4 hover:text-(--color-gold) transition-colors duration-300 leading-[1.4] flex items-center gap-[0.6rem]"
       >
         {detail.location}
-        <span className="inline-block ml-[0.6rem] text-[2rem] no-underline">
-          ↗
+        <span className="inline-block no-underline">
+          <RightDiagonalArrowIcon className="size-8" />
         </span>
       </a>
     </div>
--- src/components/time-line/index.tsx
+++ src/components/time-line/index.tsx
@@ -8,7 +8,7 @@ export default function Timeline() {
     >
       <div className="max-w-480 mx-auto">
         <div className="mb-16 sm:mb-[5.6rem]">
-          <p className="text-[0.9rem] md:text-[1.2rem] uppercase text-(--color-gold) font-medium tracking-[0.32em] font-sans">
+          <p className="text-[1rem] md:text-[1.2rem] uppercase text-(--color-gold) font-medium tracking-[0.32em] font-sans">
             Milestones
           </p>
           <h2 className="font-serif text-[clamp(3.2rem,5vw,6rem)] font-light leading-[1.1] text-(--color-text-primary) mt-[1.2rem]">
